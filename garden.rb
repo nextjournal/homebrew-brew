3eff86927495d8cbfd3b60647c689cbcd02b721e
Update <nextjournal@users.noreply.github.com>
2023-11-07 17:21:08 +0000

    Update garden CLI

--- garden.rb
+++ garden.rb
@@ -1,13 +1,15 @@
 class Garden < Formula
   desc "CLI for application.garden"
   homepage "application.garden"
-  url "https://github.com/nextjournal/garden-cli/releases/download/0.0.2/garden.tar.gz"
-  version "0.0.2"
-  sha256 "3b19f0241dd3aeb4ca0965d3424e124159cb672517899ba9be2b713a7851329b"
+  url "https://github.com/nextjournal/garden-cli/releases/download/v0.0.3/garden.tar.gz"
+  version "0.0.3"
+  sha256 "e61fee14e4d40df05b924672cd2e88c5b8c9022d286128e29686d183134431c0"
 
   depends_on "borkdude/brew/babashka"
 
-  binary "garden"
+  def install
+    bin.install "garden"
+  end
 
   test do
     system "#{bin}/garden", "--version"
