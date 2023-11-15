3ee154f79317341003a8351b3c1e5018f1bcd6a2
Update <nextjournal@users.noreply.github.com>
2023-11-15 13:34:21 +0100

    Update garden CLI cask

--- Casks/garden.rb
+++ Casks/garden.rb
@@ -1,8 +1,8 @@
 cask "garden" do
   arch arm: "aarch64", intel: "amd64"
-  version "0.0.4"
-  sha256 arm: "a522113487d562944226c6ee42010ee5f5ed53c2f0a8621a390ed294ed5f0821",
-         intel: "b5d3e0960e8299ef1be07fc87ec947fa162d16ab89505a3672a9cc8fc249624c"
+  version "0.0.5"
+  sha256 arm: "f70c56062880923c3379635a9d83efe4d04388f5f26c06d3f1d7f60e03758404",
+         intel: "fb21e42feed06ba95b5e2104e55eb7a527e21879b7367f1f5cd4044d0f39e128"
   url "https://github.com/nextjournal/garden-cli/releases/download/v#{version}/garden-macos-#{arch}.tar.gz",
       verified: "https://github.com/nextjournal/garden-cli"
   name "application.garden command line interface"
