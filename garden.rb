ac200da8e64a805b3d7df4b3d3a51238c9525a79
Update <nextjournal@users.noreply.github.com>
2024-04-11 15:05:51 +0000

    Update garden CLI

--- garden.rb
+++ garden.rb
@@ -1,23 +1,23 @@
 class Garden < Formula
   desc "CLI for application.garden"
   homepage "application.garden"
-  version "0.1.7"
+  version "0.1.8"
 
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/nextjournal/garden-cli/releases/download/v0.1.7/garden-linux-aarch64-static.tar.gz"
-      sha256 "c049bc0e3150305f42cf61ada2d7b9f394931b05bc13c840ce8db8d8555056c9"
+      url "https://github.com/nextjournal/garden-cli/releases/download/v0.1.8/garden-linux-aarch64-static.tar.gz"
+      sha256 "1a20975a41279d101c5e102dbb0543accd076023377ffca7e96dde855722cfe3"
     else
-      url "https://github.com/nextjournal/garden-cli/releases/download/v0.1.7/garden-linux-amd64-static.tar.gz"
-      sha256 "c049bc0e3150305f42cf61ada2d7b9f394931b05bc13c840ce8db8d8555056c9"
+      url "https://github.com/nextjournal/garden-cli/releases/download/v0.1.8/garden-linux-amd64-static.tar.gz"
+      sha256 "1a20975a41279d101c5e102dbb0543accd076023377ffca7e96dde855722cfe3"
     end
   else
     if Hardware::CPU.arm?
-      url "https://github.com/nextjournal/garden-cli/releases/download/v0.1.7/garden-macos-aarch64.tar.gz"
-      sha256 "42628426adb884ff0480af62c220361c7df8781de91e7c212e0cb2d6ac84a611"
+      url "https://github.com/nextjournal/garden-cli/releases/download/v0.1.8/garden-macos-aarch64.tar.gz"
+      sha256 "c9168622c0f6f0fdba4508f7831dea73a545b9288b47cc1861e3c8a013392c8a"
     else
-      url "https://github.com/nextjournal/garden-cli/releases/download/v0.1.7/garden-macos-amd64.tar.gz"
-      sha256 "1f50945f195f40e010a92a3e5810aeb74bf0a7222f3b407a541ccfd236e27a5c"
+      url "https://github.com/nextjournal/garden-cli/releases/download/v0.1.8/garden-macos-amd64.tar.gz"
+      sha256 "534860f9f215a7165cdc3166578bb1ecdca091db1caa786388c88e927937a540"
     end
   end
 
